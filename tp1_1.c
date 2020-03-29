5e19becc54a5ee421dbe85f60cab7629a52e4a20
Carlos-Lazo <carlos.lazo550@gmail.com>
2020-03-29 00:01:14 -0300

    Punto 1 y 2

---
+++ tp1_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	
+	int var=10, *p_var;
+
+	p_var = &var;
+
+	printf("Contenido del puntero: %d \n",*p_var );
+
+	printf("Direccion de memoria almacenada en el puntero: %p\n",p_var );
+
+	printf("Direccion de memoria de la variable: %p\n", &var );
+
+	printf("Direccion de memoria del puntero: %p \n",&p_var);
+
+	printf("Memoria utilizada por la variable: %i bytes\n", sizeof(var));	
+	
+	return 0;
+}
